a025b809dce3f9c32d1e4c0d5a57c938ef6cb728
Jeremie Graulle <11856386+jgraulle@users.noreply.github.com>
2019-06-18 00:16:28 +0200

    1.LCD: display a bouncing ball

--- 1.LCD/src/data_out.v
+++ 1.LCD/src/data_out.v
@@ -14,40 +14,64 @@ module data_out
 	input  wire [10:0] Y
 );
 
+localparam dotSize = 5;
+
 reg [23:0] color;
 reg [31:0] cptTime;
-reg direction;
+reg [8:0] dotX;
+reg [8:0] dotY;
+reg dirX;
+reg dirY;
 
 always@(posedge CLK)
 begin
-	if (cptTime == 32'd15_000_000)
+	if (cptTime == 32'd150_000)
 	begin
 		cptTime <= 32'b0;
-		direction <= ~direction;
+		if (dirX)
+		begin
+			if (dotX == LCD_WIDTH-dotSize-1)
+				dirX <= ~dirX;
+			else
+				dotX <= dotX + 1'b1;
+		end
+		else
+		begin
+			if (dotX == dotSize+1)
+				dirX <= ~dirX;
+			else
+				dotX <= dotX - 1'b1;
+		end
+		if (dirY)
+		begin
+			if (dotY == LCD_HEIGHT-dotSize-1)
+				dirY <= ~dirY;
+			else
+				dotY <= dotY + 1'b1;
+		end
+		else
+		begin
+			if (dotY == dotSize+1)
+				dirY <= ~dirY;
+			else
+				dotY <= dotY - 1'b1;
+		end
 	end
 	else
 		cptTime <= cptTime + 1'b1;
 
 	if (DEN)
 	begin
-		if (direction)
-		begin
-			if (X < (LCD_WIDTH/3))
-				color[23:0] <= 24'hff0000;
-			else if (X < (LCD_WIDTH/3*2))
-				color[23:0] <= 24'h00ff00;
-			else
-				color[23:0] <= 24'h0000ff;
-		end
+		if (X==0 || X == LCD_WIDTH-1 || Y==0 || Y==LCD_HEIGHT-1)
+			color[23:0] <= 24'hffffff;
+		else if (dotX-dotSize <= X && X <= dotX+dotSize && dotY-dotSize <= Y && Y <= dotY+dotSize)
+			color[23:0] <= 24'hffffff;
+		else if (X < (LCD_WIDTH/3))
+			color[23:0] <= 24'hff0000;
+		else if (X < (LCD_WIDTH/3*2))
+			color[23:0] <= 24'h00ff00;
 		else
-		begin
-			if (Y < (LCD_HEIGHT/3))
-				color[23:0] <= 24'hff0000;
-			else if (Y < (LCD_HEIGHT/3*2))
-				color[23:0] <= 24'h00ff00;
-			else
-				color[23:0] <= 24'h0000ff;
-		end
+			color[23:0] <= 24'h0000ff;
 	end
 	else
 		color <= 24'h000000;
